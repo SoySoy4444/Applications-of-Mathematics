542320ff715164d8743c61e0d6fb9a7da8751f99
GifNinja <55659559+GifNinja@users.noreply.github.com>
2019-10-13 22:36:56 +0900

    Update

--- script.js
+++ script.js
@@ -4,26 +4,41 @@
 
 var questions = [
     {
-        question : "4arcsin(x)=\pi",
+        question : "4arcsin(x)=pi",
         choiceA : "x=22sqrt(2)",
         choiceB : "x=2",
         choiceC : "$x=1/sqrt(2)",
         choiceD : "x=1/2",
-        correct : "C"
+        correct : "C",
+        answerEquation : true,
+        questionEquation: true
     }, {
         question : "8x^2-2x-3=0",
         choiceA : "x = 1/2, x=3/4",
         choiceB : "x = -1/2, x = 3/4",
         choiceC : "x = 1/2, x = -3/4",
         choiceD : "x = -1/2, x = -3/4",
-        correct : "B"
+        correct : "B",
+        answerEquation : true,
+        questionEquation: true
+    }, {
+        question : "Which of these is a complex number?",
+        choiceA : "2-i",
+        choiceB : "-30i",
+        choiceC : "pi",
+        choiceD : "All of them",
+        correct : "D",
+        answerEquation : false,
+        questionEquation: false
     }, {
         question: "Good job!",
-        choiceA : "",
-        choiceB : "",
-        choiceC: "",
-        choiceD : "",
-        correct: ""
+        choiceA : "😊",
+        choiceB : "😊",
+        choiceC: "😊",
+        choiceD : "😊",
+        correct: "😊",
+        answerEquation : false,
+        questionEquation: false
     }, {
     }
 ];
@@ -32,22 +47,40 @@ var currentQuestion = 0;
 function render(currentQuestion) {
     var choiceA = document.getElementById("A");
     /* choiceA.innerHTML = questions[currentQuestion].choiceA; */
-    choiceA.innerHTML = "<img src='https://latex.codecogs.com/svg.latex?" + questions[currentQuestion].choiceA + "' /> <br>";
+    /* choiceA.innerHTML = "<img src='https://latex.codecogs.com/svg.latex?" + questions[currentQuestion].choiceA + "' /> <br>"; */
     
     var choiceB = document.getElementById("B");
     /* choiceB.innerHTML = questions[currentQuestion].choiceB; */
-    choiceB.innerHTML = "<img src='https://latex.codecogs.com/svg.latex?" + questions[currentQuestion].choiceB + "' /> <br>";
+    /* choiceB.innerHTML = "<img src='https://latex.codecogs.com/svg.latex?" + questions[currentQuestion].choiceB + "' /> <br>"; */
     
     var choiceC = document.getElementById("C");
     /* choiceC.innerHTML = questions[currentQuestion].choiceC; */
-    choiceC.innerHTML = "<img src='https://latex.codecogs.com/svg.latex?" + questions[currentQuestion].choiceC + "' /> <br>";
+    /* choiceC.innerHTML = "<img src='https://latex.codecogs.com/svg.latex?" + questions[currentQuestion].choiceC + "' /> <br>"; */
 
     var choiceD = document.getElementById("D");
     /* choiceD.innerHTML = questions[currentQuestion].choiceD; */
-    choiceD.innerHTML = "<img src='https://latex.codecogs.com/svg.latex?" + questions[currentQuestion].choiceD + "' /> <br>";
+    /* choiceD.innerHTML = "<img src='https://latex.codecogs.com/svg.latex?" + questions[currentQuestion].choiceD + "' /> <br>"; */
     
+    if (questions[currentQuestion].answerEquation === true) {
+        choiceA.innerHTML = "<img src='https://latex.codecogs.com/svg.latex?" + questions[currentQuestion].choiceA + "' /> <br>";
+        choiceB.innerHTML = "<img src='https://latex.codecogs.com/svg.latex?" + questions[currentQuestion].choiceB + "' /> <br>";
+        choiceC.innerHTML = "<img src='https://latex.codecogs.com/svg.latex?" + questions[currentQuestion].choiceC + "' /> <br>";
+        choiceD.innerHTML = "<img src='https://latex.codecogs.com/svg.latex?" + questions[currentQuestion].choiceD + "' /> <br>";
+    } else {
+        choiceA.innerHTML = questions[currentQuestion].choiceA;
+        choiceB.innerHTML = questions[currentQuestion].choiceB;
+        choiceC.innerHTML = questions[currentQuestion].choiceC;
+        choiceD.innerHTML = questions[currentQuestion].choiceD;
+    }
+
     var question = document.getElementById("question");
-    question.innerHTML = "<img src='https://latex.codecogs.com/svg.latex?" + questions[currentQuestion].question + "' /> <br>";
+    if (questions[currentQuestion].questionEquation === true) {
+        question.innerHTML = "<img src='https://latex.codecogs.com/svg.latex?" + questions[currentQuestion].question + "' /> <br>";
+    } else {
+        question.innerHTML = questions[currentQuestion].question;
+    }
+    /* it can only accept equation problems, so use an IF statement to filter "if this is equation" then latex "else" "string". */
+    
 }
 
 function check(answer) {
