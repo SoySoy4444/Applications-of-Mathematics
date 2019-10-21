fe48ec133aa9ae64078fba31bd81d92a5566d253
GifNinja <55659559+GifNinja@users.noreply.github.com>
2019-10-21 19:29:48 +0900

    Tax Fraud Update
    Added Benford Excel spreadsheet 
    Added Benford "Try It Yourself" JavaScript

---
+++ benford.js
@@ -0,0 +1,104 @@
+/*jslint plusplus: true */
+
+"use strict";
+
+function benford() {
+    var integers = document.getElementById("numbersInput").value,
+        integersList = integers.split("\n"),
+        counts = [],
+        i = 0;
+/* 
+    alert(integersList[0]);
+    alert(integersList[1]);
+    alert(integersList[integersList.length - 1]);
+*/
+
+    for (i; i < integersList.length; i += 1) {
+        counts.push(integersList[i][0]);
+    }
+    
+    i = 0;
+    var totalCount = counts.length,
+        one = 0,
+        two = 0,
+        three = 0,
+        four = 0,
+        five = 0,
+        six = 0,
+        seven = 0,
+        eight = 0,
+        nine = 0;
+
+    for (i; i < counts.length; i += 1) {
+        
+        switch (Number(counts[i])) {
+        case 1:
+            one++;
+            break;
+        case 2:
+            two++;
+            break;
+        case 3:
+            three++;
+            break;
+        case 4:
+            four++;
+            break;
+        case 5:
+            five++;
+            break;
+        case 6:
+            six++;
+            break;
+        case 7:
+            seven++;
+            break;
+        case 8:
+            eight++;
+            break;
+        case 9:
+            nine++;
+            break;
+        }
+    }
+    
+    document.getElementById("counts").innerHTML =
+    `
+    One appeared ${one} times <br>
+    Two appeared ${two} times <br>
+    Three appeared ${three} times <br>
+    Four appeared ${four} times <br>
+    Five appeared ${five} times <br>
+    Six appeared ${six} times <br>
+    Seven appeared ${seven} times <br>
+    Eight appeared ${eight} times <br>
+    Nine appeared ${nine} times <br>
+    `;
+    
+    document.getElementById("probabilities").innerHTML =
+    `
+    One appeared ${(one/totalCount*100).toFixed(2)}% of the time <br>
+    Two appeared ${(two/totalCount*100).toFixed(2)}% of the time <br>
+    Three appeared ${(three/totalCount*100).toFixed(2)}% of the time <br>
+    Four appeared ${(four/totalCount*100).toFixed(2)}% of the time <br>
+    Five appeared ${(five/totalCount*100).toFixed(2)}% of the time <br>
+    Six appeared ${(six/totalCount*100).toFixed(2)}% of the time <br>
+    Seven appeared ${(seven/totalCount*100).toFixed(2)}% of the time <br>
+    Eight appeared ${(eight/totalCount*100).toFixed(2)}% of the time <br>
+    Nine appeared ${(nine/totalCount*100).toFixed(2)}% of the time <br>
+    `;
+    
+    /*
+    alert(one);
+    alert(two);
+    alert(three);
+    alert(four);
+    alert(five);
+    alert(six);
+    alert(seven);
+    alert(eight);
+    alert(nine);
+    */
+    
+}
+    
